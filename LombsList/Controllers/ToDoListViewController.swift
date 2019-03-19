a5550254b2d2f8f188fff9e14e9dd3ca72b752a9
Nsonsa <elombekisala@gmail.com>
2019-03-19 06:50:46 -0400

    Organized MVC files. Refactored code, got rid of Strings in itemArray and instead populated the array with item objects. Appended 3 items to the array. Changed table view methods to work with item objects. Reloaded table data every time a change to the table cells were made. Refactored conditional statements in table view methods. Learned about ternary operators.

--- LombsList/Controllers/ToDoListViewController.swift
+++ LombsList/Controllers/ToDoListViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["Shopping", "Oil Change", "Pickup Nachos"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items  = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
-        }
+        let newItem = Item()
+        newItem.title = "Go Ski"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Pick Up Nachos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Sleep"
+        itemArray.append(newItem3)
+        
+        
+//        if let items  = defaults.array(forKey: "ToDoListArray") as? [String] {
+//            itemArray = items
+//        }
     }
 
     //MARK - Tableview Datasource Methods
@@ -31,8 +44,25 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let item = itemArray[indexPath.row]
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        
+//        // if row selected was already true
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
+        
+        // Ternery operator
+        // value = condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
         
         return cell
     }
@@ -40,12 +70,19 @@ class ToDoListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        //Row was selected
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        // check if row was selected
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
+        
+        // Refactored code above
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        // Update data
+        tableView.reloadData()
+        
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -61,7 +98,10 @@ class ToDoListViewController: UITableViewController {
         let actionButton = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             // User clicked add item button on UIAlert
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
---
+++ LombsList/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  LombsList
+//
+//  Created by Nsonsa Elombe Kisala Jr. on 3/19/19.
+//  Copyright © 2019 Nsonsa Kisala Jr. All rights reserved.
+//
+
+import UIKit
+class Item {
+    var title : String = ""
+    var done :  Bool = false
+}
